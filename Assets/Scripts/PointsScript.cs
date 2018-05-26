41888447e9f4bfa724d0f065d90d8d61a8247057
dani5636 <dani5636@easv365.dk>
2018-05-26 15:56:35 +0200

    Rotation of notes. Shortscuts Pause

---
+++ Assets/Scripts/PointsScript.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class PointsScript : MonoBehaviour {
+
+    public static int scoreValue = 0;
+    TextMeshProUGUI points;
+
+    void Start() {
+
+        points = GetComponent<TextMeshProUGUI>();
+
+    }
+
+
+    void Update() {
+        points.text = "" + scoreValue;
+
+        if (Input.GetKeyDown(KeyCode.T))
+        {
+            scoreValue += 10;
+        }
+    }
+
+    
+}
