034d59d62e618da79a76bd52e8c944c3dde2fe74
Fjord82 <Fjordrasmus@gmail.com>
2018-05-26 17:14:32 +0200

    Fading anim on audiosource and scene

---
+++ Assets/LevelChanger.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelChanger : MonoBehaviour {
+
+    public Animator animator;
+
+    private int levelToLoad;
+
+    [SerializeField]
+    private float fadeOutTime = 3.0f;
+
+    [SerializeField]
+    private GameObject audioObject;
+    AudioSource audioSource;
+    bool isFading;
+    float fadePerSec;
+
+    private void Start()
+    {
+        audioSource = audioObject.GetComponent<AudioSource>();
+    }
+
+
+    // Update is called once per frame
+    void Update () {
+        if (Input.GetKeyDown(KeyCode.L))
+        {
+            FadeToNextLevel();
+            isFading = true;
+            fadePerSec = audioSource.volume / fadeOutTime;
+
+        }
+        if(isFading){
+                audioSource.volume = Mathf.MoveTowards(
+                    audioSource.volume, 0, fadePerSec * Time.deltaTime);
+            
+        }
+	}
+
+    public void FadeToNextLevel ()
+    {
+        FadeToLevel(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void FadeToLevel(int levelIndex)
+    {
+        levelToLoad = levelIndex;
+        animator.SetTrigger("FadeOut");
+    }
+
+    public void OnFadeComplete ()
+    {
+        SceneManager.LoadScene(levelToLoad);
+
+    }
+
+   
+}
