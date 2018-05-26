5b488d22d6391bb527943b18780055298ad38b55
dani5636 <dani5636@easv365.dk>
2018-05-26 17:19:44 +0200

    Final Scene has score

---
+++ Assets/Scripts/HealthBar.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthBar : MonoBehaviour {
+
+    Image healthBar;
+    private float speed = 0.01f;
+    private bool running;
+ 
+
+    void Start () {
+
+        healthBar = GetComponent<Image>();
+
+        healthBar.fillAmount = 0.5f;
+
+        running = false;
+    }
+	
+	
+	void Update () {
+
+        if (running)
+        {
+            
+            DecreasingHealth();
+
+        }
+        
+       
+
+
+
+    }
+    public void DecreasingHealth()
+    {
+        healthBar.fillAmount -= Time.deltaTime * speed;
+    }
+
+    public void HealthIncrease()
+    {
+        healthBar.fillAmount += 0.05f;
+    }
+    public void setRunning(bool running) {
+        this.running = running;
+    }
+    public float getHealth() {
+        return healthBar.fillAmount;
+    }
+    public void GameOver()
+    {
+        if (healthBar.fillAmount == 0)
+        {
+
+        }
+
+    }
+}
