521f167ecb74c93aeed7d793d48807a8aedd2bf8
Fjord82 <Fjordrasmus@gmail.com>
2018-05-28 12:06:42 +0200

    animation glass opener on lightsabercaskets

---
+++ Assets/casketOpener.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class casketOpener : MonoBehaviour {
+    
+    private Vector3 originalScale;
+
+    private float limitMinScale = 0.1f;
+    bool isScaling = false;
+
+	// Use this for initialization
+	void Start () {
+        originalScale = transform.localScale;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        ScalingObject();
+        if (Input.GetKeyDown(KeyCode.DownArrow))
+        { 
+            isScaling = true;
+        }
+
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            isScaling = false;
+        }
+    }
+
+    private void ScalingObject()
+    {
+
+
+        if (isScaling && transform.localScale.y >= limitMinScale)
+        {
+            transform.localScale += new Vector3(0, -0.01f, 0);
+        }
+
+
+        if (!isScaling && transform.localScale != originalScale)
+            {
+                transform.localScale += new Vector3(0, 0.01f, 0);
+            }
+        
+    }
+}
